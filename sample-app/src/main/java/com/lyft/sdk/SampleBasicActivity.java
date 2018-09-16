8852099f530fad1cb15f70a321ffc236738e5b11
Jeremy Xue <jhxue@andrew.cmu.edu>
2018-09-16 03:21:01 -0400

    mvp

--- sample-app/src/main/java/com/lyft/sdk/SampleBasicActivity.java
+++ sample-app/src/main/java/com/lyft/sdk/SampleBasicActivity.java
@@ -6,6 +6,7 @@ import com.lyft.lyftbutton.LyftButton;
 import com.lyft.lyftbutton.RideParams;
 import com.lyft.lyftbutton.RideTypeEnum;
 import com.lyft.networking.ApiConfig;
+import android.content.Intent;
 
 public class SampleBasicActivity extends Activity {
 
@@ -15,20 +16,29 @@ public class SampleBasicActivity extends Activity {
 
         setContentView(R.layout.activity_sample_basic);
 
-        ApiConfig apiConfig = new ApiConfig.Builder()
-                .setClientId("z3Q_D3hYmpmw")
-                .setClientToken("CzJYudFVOtGCSC8Qz++cQ9iF04VMkcuEf5g1sngFXIhQu2yjKWVvQaTGd+77GJ8jXruzrDMKwjMBFksDg84yMo319Xd9hLa1YOp2Fx8ZXGvVU3L0hKnHAIQ=")
-                .build();
 
-        LyftButton lyftButton = (LyftButton) findViewById(R.id.lyft_button);
-        lyftButton.setApiConfig(apiConfig);
+        //ApiConfig apiConfig = new ApiConfig.Builder()
+        //        .setClientId("z3Q_D3hYmpmw")
+        //        .setClientToken("CzJYudFVOtGCSC8Qz++cQ9iF04VMkcuEf5g1sngFXIhQu2yjKWVvQaTGd+77GJ8jXruzrDMKwjMBFksDg84yMo319Xd9hLa1YOp2Fx8ZXGvVU3L0hKnHAIQ=")
+        //        .build();
+//
+        //LyftButton lyftButton = (LyftButton) findViewById(R.id.lyft_button);
+        //lyftButton.setApiConfig(apiConfig);
+//
+        //RideParams.Builder rideParamsBuilder = new RideParams.Builder()
+        //        .setPickupLocation(37.7766048, -122.3943629)
+        //        .setDropoffLocation(37.759234, -122.4135125);
+        //rideParamsBuilder.setRideTypeEnum(RideTypeEnum.CLASSIC);
+//
+        //lyftButton.setRideParams(rideParamsBuilder.build());
+        //lyftButton.load();
 
-        RideParams.Builder rideParamsBuilder = new RideParams.Builder()
-                .setPickupLocation(37.7766048, -122.3943629)
-                .setDropoffLocation(37.759234, -122.4135125);
-        rideParamsBuilder.setRideTypeEnum(RideTypeEnum.CLASSIC);
+        //Function that reads the alcohol sensor data
+        //If the sensor returns a 2 at any point,
+        if(false) {
+            Intent menuIntent = new Intent(this, SampleLocationAwareActivity.class);
+            startActivity(menuIntent);
+        }
 
-        lyftButton.setRideParams(rideParamsBuilder.build());
-        lyftButton.load();
     }
 }
--- sample-app/src/main/java/com/lyft/sdk/SampleLocationAwareActivity.java
+++ sample-app/src/main/java/com/lyft/sdk/SampleLocationAwareActivity.java
@@ -31,6 +31,7 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
+
 public class SampleLocationAwareActivity extends Activity {
 
     private static final int PERMISSION_REQUEST_CODE = 1336;
